f21fafdeeba62e2dc7fcccb4eff88b2b6cfc249c
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2025-03-11 23:43:37 +0530

    Update MessagesRoute.js

--- server/routes/MessagesRoute.js
+++ server/routes/MessagesRoute.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getMessages, uploadFile } from '../models/MessageController.js';
+import { getMessages } from '../models/MessageController.js';
 import { verifyToken } from '../middlewares/AuthMiddleware.js'
 import multer from 'multer';
+// import { uploadFile } from '../models/MessageController.js';
 
 const MessagesRoute = Router();
-const uploads = multer({ dest: 'uploads/files' });
+// const uploads = multer({ dest: 'uploads/files' });
 MessagesRoute.post("/get-messages", verifyToken , getMessages);
 MessagesRoute.post("/upload-file", verifyToken, uploads.single('file'), uploadFile);
 
